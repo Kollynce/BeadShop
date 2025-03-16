9bb9ced990a1391caffa13d17c90c3a7ae3ef3b7
kollynce <collinsodeny@gmail.com>
2025-03-16 09:49:35 +0300

    update sitemap URLs to reflect new domain

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-  // ...existing code...
-  
   // This is important for GitHub Pages to work with Vue Router
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   
   // If you're using a custom domain, leave publicPath as '/'
   // If you're using GitHub's default domain (username.github.io/repo-name),
   // then set publicPath to '/repo-name/'
-  
-  // ...existing code...
 }
