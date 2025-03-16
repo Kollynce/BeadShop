93ff593df42de888b3ca8cec36e774717bde11c0
kollynce <collinsodeny@gmail.com>
2025-03-16 11:26:47 +0300

    add 404.html redirect to deployment script for improved user experience

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,12 @@ cd dist
 # Create a .nojekyll file to prevent GitHub Pages from ignoring files that begin with an underscore
 touch .nojekyll
 
+# create a 404.html file that redirects to index.html
+cp index.html 404.html
+
 # if you are deploying to a custom domain
 echo 'kreativekanvas.shop' > CNAME
 
-# Initialize git if this isn't already a git repo
 git init
 git add -A
 git commit -m 'deploy'
